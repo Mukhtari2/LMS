e67cd9f7e2a7647e2697b8a3e1fdd662c49582b5
BARHAMA <suleimannmukhtar@gmail.com>
2026-01-09 12:59:34 +0100

    changed my database to mongodb

--- src/main/java/com/example/LearningManagementSystem/controller/StudentController.java
+++ src/main/java/com/example/LearningManagementSystem/controller/StudentController.java
@@ -1,31 +1,31 @@
-//package com.example.LearningManagementSystem.controller;
-//
-//import com.example.LearningManagementSystem.dto.*;
+package com.example.LearningManagementSystem.controller;
+
+import com.example.LearningManagementSystem.dto.*;
 //import com.example.LearningManagementSystem.service.AssignmentService;
-//import com.example.LearningManagementSystem.service.EnrollmentService;
+import com.example.LearningManagementSystem.service.EnrollmentService;
 //import com.example.LearningManagementSystem.service.SubmissionService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/v1/student")
-//@PreAuthorize("hasRole('STUDENT')")
-//@RequiredArgsConstructor
-//public class StudentController {
-//
-//    private final EnrollmentService enrollmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/student")
+@PreAuthorize("hasRole('STUDENT')")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final EnrollmentService enrollmentService;
 //    private final SubmissionService submissionService;
 //    private final AssignmentService assignmentService;
-//
-//    @PostMapping("/enroll")
-//    public ResponseEntity<EnrollmentResponseDTO> enrolPublishedCourse(@RequestBody EnrollmentRequestDTO enrollmentRequestDTO){
-//        EnrollmentResponseDTO enroll = enrollmentService.enrollPublishedCourse(enrollmentRequestDTO);
-//        return new ResponseEntity<>(enroll, HttpStatus.CREATED);
-//    }
-//
+
+    @PostMapping("/enroll")
+    public ResponseEntity<EnrollmentResponseDTO> enrolPublishedCourse(@RequestBody EnrollmentRequestDTO enrollmentRequestDTO) {
+        EnrollmentResponseDTO enroll = enrollmentService.enrollPublishedCourse(enrollmentRequestDTO);
+        return new ResponseEntity<>(enroll, HttpStatus.CREATED);
+    }
+
 //    @PostMapping("/submit-answer")
 //    public ResponseEntity<SubmissionResponseDTO>  submitAnswer(@RequestBody SubmissionRequestDTO submissionRequestDTO){
 //        SubmissionResponseDTO responseDTO = submissionService.submitAnswers(submissionRequestDTO);
@@ -37,7 +37,7 @@
 //        AssignmentResponseDTO responseDTO = assignmentService.viewAssignment(assignmentId);
 //        return ResponseEntity.ok(responseDTO);
 //    }
-//
-//
-//
-//}
+
+
+
+}
--- src/main/java/com/example/LearningManagementSystem/repository/EnrollmentRepository.java
+++ src/main/java/com/example/LearningManagementSystem/repository/EnrollmentRepository.java
@@ -1,10 +1,10 @@
-//package com.example.LearningManagementSystem.repository;
-//
-//import com.example.LearningManagementSystem.model.Enrollment;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface EnrollmentRepository extends MongoRepository<Enrollment, Long> {
-//
-//}
+package com.example.LearningManagementSystem.repository;
+
+import com.example.LearningManagementSystem.model.Enrollment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnrollmentRepository extends MongoRepository<Enrollment, Long> {
+
+}
