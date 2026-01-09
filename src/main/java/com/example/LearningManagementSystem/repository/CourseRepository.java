1a01593b1f29d92bfe19adf7fbd0f9adfe63ee72
BARHAMA <suleimannmukhtar@gmail.com>
2026-01-09 12:56:50 +0100

    changed my database to mongodb

--- src/main/java/com/example/LearningManagementSystem/repository/CourseRepository.java
+++ src/main/java/com/example/LearningManagementSystem/repository/CourseRepository.java
@@ -1,12 +1,12 @@
-//package com.example.LearningManagementSystem.repository;
-//
-//import com.example.LearningManagementSystem.model.Course;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//public interface CourseRepository extends MongoRepository<Course, Long> {
-//    Optional<Course> findById(Long courseId);
-//}
+package com.example.LearningManagementSystem.repository;
+
+import com.example.LearningManagementSystem.model.Course;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CourseRepository extends MongoRepository<Course, Long> {
+    Optional<Course> findById(Long courseId);
+}
--- src/main/java/com/example/LearningManagementSystem/repository/LessonRepository.java
+++ src/main/java/com/example/LearningManagementSystem/repository/LessonRepository.java
@@ -1,12 +1,12 @@
-//package com.example.LearningManagementSystem.repository;
-//
-//import com.example.LearningManagementSystem.model.Lesson;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//public interface LessonRepository extends MongoRepository<Lesson, Long> {
-//    Optional<Lesson> findById(Long lessonId);
-//}
+package com.example.LearningManagementSystem.repository;
+
+import com.example.LearningManagementSystem.model.Lesson;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LessonRepository extends MongoRepository<Lesson, Long> {
+    Optional<Lesson> findById(Long lessonId);
+}
