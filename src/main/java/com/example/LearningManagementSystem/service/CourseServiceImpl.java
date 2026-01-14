259cdd6c88a28ee8d6502ed972481e22faacafeb
BARHAMA <suleimannmukhtar@gmail.com>
2026-01-14 21:10:16 +0100

    progress

--- src/main/java/com/example/LearningManagementSystem/service/CourseServiceImpl.java
+++ src/main/java/com/example/LearningManagementSystem/service/CourseServiceImpl.java
@@ -28,6 +28,7 @@ public class CourseServiceImpl implements CourseService{
                 .title(requestDTO.getTitle())
                 .teacherId(requestDTO.getTeacherId())
                 .role(Role.TEACHER)
+
                 .build();
         Course newCourse = repository.insert(course);
         return courseMapper.toDto(newCourse);
